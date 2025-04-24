69a27bf5634f07a0117e0ff4683a0e3f8d4559a5
kanyecat <89665006+kanyecat@users.noreply.github.com>
2025-04-24 13:14:03 -0700

    Update About.jsx

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -13,54 +13,71 @@ const About = () => {
               <div className='flex flex-row pb-6 gap-4'>
                 <span className='text-end md:text-[14px] text-[12px]'>TITLE</span>
                 <div className='flex flex-col'>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Korean National Artist, 2006, 2007, 2009.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Seoul GANG-NAM Ministry of Culture,  2008. </span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>National Christian Art Competition, 2013.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-3`}>Korean National Artist</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2013 National Christian Art Competition, Seoul, South Korea.
+                    <em> Art Juror Qualification.</em>
+                  </span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2008 Seoul GANG-NAM Ministry of Culture, Seoul, South Korea.
+                  <em> Art Juror Qualification.</em>
+                  </span>
                 </div>              
               </div>
-              <div className='flex flex-row pb-6 gap-4'>
+              {/* <div className='flex flex-row pb-6 gap-4'>
                 <span className='text-end md:text-[14px] text-[12px]'>PEN NAME</span>
                 <div className='flex flex-col'>
                   <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Saegil</span>
                   <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Soji</span>
                   <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Sorim</span>
                 </div>
-              </div>
+              </div> */}
               <div className='flex flex-row pb-6 gap-4'>
                 <span className='text-end md:text-[14px] text-[12px]'>EDUCATION</span>
                 <div className='flex flex-col'>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px]`}>Sungshin Women's University</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Department of Oriental Painting</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px]`}>1993 Sungshin Women’s University, Seoul, South Korea.
+                    <em> Bachelor of Fine Arts, Department of Oriental Painting.</em>
+                  </span>
                 </div>              
               </div>
               <div className='flex flex-row pb-6 gap-4'>
                 <span className='text-end md:text-[14px] text-[12px]'>AWARDS</span>
                 <div className='flex flex-col'>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Korea SEO-DO Association, Chinese Writing Junior/HIGH 1st Place, 1988.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Korea SEO-DO Association, Chinese Writing Junior/HIGH Gold Medal (Korean Writing Special Prize), 1989.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>JUNG-BU Calligraphy Competition, Korean Writing Special Prize, 2005.</span>      
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Asia Calligraphy/Painting in the Literary Artist's Style Competition, Korean Writing Gold Medal, 2006.</span>      
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>National Calligraphy Competition, Korean Writing Accepted 3 times, 2006, 2007, 2009.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>GANG-NAM Calligraphy Competition, Korean Writing 2nd Place & Special Prize, 2005~2007.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>GANG-NAM Calligraphy Competition, Korean Writing Guest Artist, 2008.</span>               
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2024 Korea In Color: A Legacy of Auspicious Images, The San Diego Museum of Art,
+                  San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2017 Saegil Group Exhibition, San Diego Rancho Penasquitos Library, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2016 Saegil Solo Exhibition, TOM N TOM'S, San Diego, CA.</span>      
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2016 Saegil Group Exhibition, San Diego Korean Library, San Diego, CA.</span>      
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2015 Saegil Group Exhibition, San Diego Korean Library, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2014 Saegil Group Exhibition, San Diego Korean Library, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2009 BO-REUM, Dongan Cultural Center, Gyeonggi-do, South Korea.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2007 BO-REUM, Dongan Cultural Center, Gyeonggi-do, South Korea.</span>    
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2005 BO-REUM, Anyang Arts Center, Gyeonggi-do, South Korea..</span>                   
                 </div>              
               </div>
               <div className='flex flex-row pb-6 gap-4'>
-                <span className='text-end md:text-[14px] text-[12px]'>FIELD EXPERIENCE</span>
+                <span className='text-end md:text-[14px] text-[12px]'>VISITING ARTIST LECTURES/PERFORMANCES</span>
                 <div className='flex flex-col'>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>San Diego Bishop School Special Lecture & Performance, 2011, 2012.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>UCSD Special Lecture, 2012.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>SDSU Special Performance, 2012.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Korean Silver College Calligraphy & Oriental Painting Lecture, 2011~2017.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>San Diego Adult, Junior/High Calligraphy & Oriental Painting & Korean Folk Art Lesson, 2011~2017.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>AMASE (special education) Art Director, 2012~2013.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Barnard Chinese Elementary School Calligraphy & Oriental Painting Art Class Teacher, 2013.</span>             
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Saegil Member's First Exhibition, 2014.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Saegil Member's Second Exhibition, 2015.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Annual Linda Vista Multi-Cultural Fair & Parade, 2015~2019.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Saegil Member's Third Exhibition, 2016.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Saegil Individual Exhibition TOM N TOM'S Support, 2016.</span>
-                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>Hand In Hand (special education) Art Teacher, 2017~2019.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2013 Guest Artist, Barnard Mandarin Magnet Elementary School, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2012 Special Performance, SDSU, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2012 Special Lecture, UCSD, La Jolla, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2012 Special Lecture & Performance, The Bishop’s School, La Jolla, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2011 Special Lecture & Performance, The Bishop’s School, La Jolla, CA.</span>
+                </div>              
+              </div>
+              <div className='flex flex-row pb-6 gap-4'>
+                <span className='text-end md:text-[14px] text-[12px]'>VOLUNTEER SERVICE</span>
+                <div className='flex flex-col'>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2017–2019 Art Director, Hand In Hand (special education), San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2015–2019 Artist, Annual Linda Vista Multi-Cultural Fair & Parade, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2012–2013 Art Director, AMASE (special education), San Diego, CA.</span>             
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2012–2013 Art Instructor, Silver College at Korean Calvary Church, San Diego, CA.</span>
+                </div>              
+              </div>
+              <div className='flex flex-row pb-6 gap-4'>
+                <span className='text-end md:text-[14px] text-[12px]'>TEACHING EXPERIENCE</span>
+                <div className='flex flex-col'>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2023–Present Instructor, Culture Center at Korean Calvary Church, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2011–Present Instructor, Private Group Lesson, San Diego, CA.</span>
+                  <span className={`flex-[2.5] font-thin md:text-[16px] text-[16px] pb-2`}>2011–2017 Instructor, The San Diego Korean American Senior Center, San Diego, CA.</span>
                 </div>              
               </div>
             </div>
